14fb50ea45b4f5e05b0bf53cd0014da677f688f1
aizat221122 <aizat.tukeweva@icloud.com>
2025-05-22 22:52:48 +0300

    Delete teacher_crm_netlify directory

--- teacher_crm_netlify/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Главная</h1>
-      <p>Выберите роль, чтобы войти: <a href="/register">Регистрация</a></p>
-    </div>
-  );
-}
